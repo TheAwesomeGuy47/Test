24c46c52a7f90de59078582e02c99ddea1ab32aa
Noinkin <87577662+Noinkin@users.noreply.github.com>
2021-07-31 08:18:43 +0100

    Update spawn.js

--- scripts/modules/minecraft/commands/test/spawn.js
+++ scripts/modules/minecraft/commands/test/spawn.js
@@ -13,5 +13,29 @@ const registerInformation = {
 };
 
 Command.register(registerInformation, (chatmsg, args) => {
-    runCommand(`tp "${chatmsg.sender.name}" 0 100 0`);
+    const player = chatmsg.sender
+    const location = player.location
+    if(player.location == location) {
+      setTimeout(myFunction, 1000)
+      function myFunction() {
+        if(player.location == location) {
+          setTimeout(myFunction1, 1000)
+          function myFunction1() {
+            if(player.location == location) {
+              setTimeout(myFunction2, 1000)
+              function myFunction2() {
+                if(player.location == location) {
+                  setTimeout(myFunction3, 1000)
+                  function myFunction4() {
+                    if(player.location == location) {
+                      runCommand(`tp "${chatmsg.sender.name}" 0 100 0`);
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
 });
