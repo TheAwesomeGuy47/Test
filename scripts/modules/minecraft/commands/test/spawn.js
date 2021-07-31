ac0acb1855e763584305ed006804cb91f7a24c99
Noinkin <87577662+Noinkin@users.noreply.github.com>
2021-07-31 08:33:47 +0100

    Update spawn.js

--- scripts/modules/minecraft/commands/test/spawn.js
+++ scripts/modules/minecraft/commands/test/spawn.js
@@ -16,19 +16,27 @@ Command.register(registerInformation, (chatmsg, args) => {
     const player = chatmsg.sender
     const location = player.location
     if(player.location == location) {
+      runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":{["text":"Do not move for 5 seconds to be teleported to spawn"}]}`)
       setTimeout(myFunction, 1000)
       function myFunction() {
         if(player.location == location) {
+          runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":{["text":"Do not move for 4 seconds to be teleported to spawn"}]}`)
           setTimeout(myFunction1, 1000)
           function myFunction1() {
             if(player.location == location) {
+              runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":{["text":"Do not move for 3 seconds to be teleported to spawn"}]}`)
               setTimeout(myFunction2, 1000)
               function myFunction2() {
                 if(player.location == location) {
+                  runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":{["text":"Do not move for 2 seconds to be teleported to spawn"}]}`)
                   setTimeout(myFunction3, 1000)
-                  function myFunction4() {
+                  function myFunction3() {
+                    runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":{["text":"Do not move for 1 second to be teleported to spawn"}]}`)
                     if(player.location == location) {
-                      runCommand(`tp "${chatmsg.sender.name}" 0 100 0`);
+                      setTimeout(myFunction4, 1000)
+                      function myFunction4() {
+                        runCommand(`tp "${chatmsg.sender.name}" 0 100 0`);
+                      }
                     }
                   }
                 }
